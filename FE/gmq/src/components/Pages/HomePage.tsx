1af3eb5c698fc407a88780ba4357fe18db017aac
Neyko Naydenov <naydenovneyko@gmail.com>
2024-12-04 16:44:45 +0200

    made button on menu center part 1 center div

--- FE/gmq/src/components/Pages/HomePage.tsx
+++ FE/gmq/src/components/Pages/HomePage.tsx
@@ -1,9 +1,15 @@
+import './PlayButton.css'
 
+function handleClick()
+{
+    console.log("Clicked!");
+}
 export const Home = (): JSX.Element => {
-    return(
-        <div>
-        <h1>Home Page Here</h1>
-        <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus ipsa, animi velit ea reprehenderit enim qui voluptatum impedit ad soluta tenetur nihil quas eligendi dolor iusto iste ullam vero tempore.</h2>
+    return (
+        <div className="BtnPos" >
+        <button className="PlayBtn" onClick={handleClick}>
+            Play
+        </button>
         </div>
-    )
+        );
 }
