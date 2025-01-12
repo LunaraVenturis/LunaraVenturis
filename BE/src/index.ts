9541056efc24d119b628a11003d7132e9307f2f3
raMitov <chizmywoe@gmail.com>
2025-01-12 22:23:53 +0200

    breakpoint

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -6,6 +6,8 @@ App.get('/', (req, res) => {
     res.send('Express + TypeScript Server');
   });
   
+
+  
   App.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
   });
